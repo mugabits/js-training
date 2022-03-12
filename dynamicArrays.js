6f0f8d696ba1c85f5bfb297cae40b82ab4ad270d
josem <jose.mugaburu@ge.com>
2022-03-11 21:29:33 -0500

    new approach for dynamic array

--- dynamicArrays.js
+++ dynamicArrays.js
@@ -1,5 +1,25 @@
 function productOfDynamicArrays(arr) {
     let before = 1;
+    const products = [];
+
+    // befores -> [1,1,2,6]
+    for (let i=0; i < arr.length; i++) {
+        products[i] = before;
+        before *= arr[i];
+    }
+
+    let after = 1;
+    for (let k=arr.length -1; k > -1; k--) {
+        products[k] = after * products[k];
+        after *= arr[k];
+    }
+
+    return products;
+}
+
+
+function productOfDynamicArraysGreedy(arr) {
+    let before = 1;
     const beforeProduct = [];
 
 
@@ -33,5 +53,5 @@ function productOfDynamicArrays(arr) {
 
 
 console.log(productOfDynamicArrays([1,2,3,4])) // [24,12,8,6]
-// console.log(productOfDynamicArrays([5,8,7,10])) // [560,350,400,280]
-// console.log(productOfDynamicArrays([0,1,2,4])) // [8,0,0,0]
+console.log(productOfDynamicArrays([5,8,7,10])) // [560,350,400,280]
+console.log(productOfDynamicArrays([0,1,2,4])) // [8,0,0,0]
